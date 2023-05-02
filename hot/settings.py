8d49f8a846b84fdb7ca9f30951018d4a0ef48ef8
patotec <ewaenpatrick5@gmail.com>
2023-05-02 08:25:30 +0100

    first

--- hot/settings.py
+++ hot/settings.py
@@ -91,13 +91,13 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-# import dj_database_url
+import dj_database_url
 
-# DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-# DATABASES = {
-#     "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
-# }
+DATABASES = {
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+}
 
 
 # Password validation
